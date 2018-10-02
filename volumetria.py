776fa86e49d86a2413840fa03e14806cb93ecf07
2maykealex <mayke.suporte@gmail.com>
2018-10-02 10:00:20 -0400

    Projeto Volumetria Inicio

---
+++ volumetria.py
@@ -0,0 +1,195 @@
+'''
+projeto   deeplaw: volumetria
+automatizar a inserção da volumetria em pastas existentes no sistema dplaw
+'''
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import *
+import os
+import time
+# import pandas as pd
+import pyexcel as pe
+import datetime
+import locale
+
+# TODO DEIXAR O CÓDIGO MAIS LIMPO DISTRIBUINDO FUNÇÕES PARA OUTROS ARQUIVOS
+
+def iniciaWebdriver():
+    # acessando diretório do webdriver do chrome
+    dirpath = os.path.dirname(os.path.realpath(__file__))
+    chromepath = dirpath + '/chromedriver'
+    driver = webdriver.Chrome(executable_path = chromepath)
+    return driver
+
+def waitinstance(browser, object, time, poll, type, form = 'xpath'):
+    if type == 'click':
+        if form == 'xpath':
+            element = WebDriverWait(browser, time, poll_frequency = poll,
+                                    ignored_exceptions=[NoSuchElementException,
+                                    ElementNotVisibleException, ElementNotSelectableException]).until(EC.element_to_be_clickable((By.XPATH, object)))
+            return element
+        elif form == 'id':
+            element = WebDriverWait(browser, time, poll_frequency = poll,
+                                    ignored_exceptions=[NoSuchElementException,
+                                    ElementNotVisibleException, ElementNotSelectableException]).until(EC.element_to_be_clickable((By.ID, object)))
+            return element
+    elif type == 'show':
+        if form == 'xpath':
+            element = WebDriverWait(browser, time, poll_frequency = poll,
+                                    ignored_exceptions=[NoSuchElementException,
+                                    ElementNotVisibleException, ElementNotSelectableException]).until(EC.presence_of_element_located((By.XPATH, object)))
+            return element
+        elif form == 'id':
+            element = WebDriverWait(browser, time, poll_frequency = poll,
+                                    ignored_exceptions=[NoSuchElementException,
+                                    ElementNotVisibleException, ElementNotSelectableException]).until(EC.presence_of_element_located((By.ID, object)))
+            return element
+
+    if responsavel == 'acordosg4':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-1"]'
+    elif responsavel == 'ADV1GE':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-2"]'
+    elif responsavel == 'ADV2GE':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-3"]'
+    elif responsavel == 'ADV3GE':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-3"]'
+    elif responsavel == 'ADV4GE':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-4"]'
+    elif responsavel == 'advbradesco':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-5"]'
+    elif responsavel == 'advgg1':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-6"]'
+    elif responsavel == 'AG6':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-7"]'
+    elif responsavel == 'AGE':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-8"]'
+    elif responsavel == 'AGE2':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-9"]'
+    elif responsavel == 'AGS':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-10"]'
+    elif responsavel == 'ApoioBV':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-11"]'
+    elif responsavel == 'apoiogg1':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-12"]'
+    elif responsavel == 'apoiogg2':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-13"]'
+    elif responsavel == 'BVADV1':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-14"]'
+    elif responsavel == 'BVADV2':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-15"]'
+    elif responsavel == 'cbradesco':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-16"]'
+    elif responsavel == 'CBV':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-17"]'
+    elif responsavel == 'CGE':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-18"]'
+    elif responsavel == 'COBRA1':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-19"]'
+    elif responsavel == 'COP':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-20"]'
+    elif responsavel == 'Correspondente':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-21"]'
+    elif responsavel == 'CSEG':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-22"]'
+    elif responsavel == 'ESP':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-23"]'
+    elif responsavel == 'EXT':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-24"]'
+    elif responsavel == 'Financeiro':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-25"]'
+    elif responsavel == 'GFP':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-26"]'
+    elif responsavel == 'GG1':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-27"]'
+    elif responsavel == 'GG2':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-28"]'
+    elif responsavel == 'GST':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-29"]'
+    elif responsavel == 'JEP':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-30"]'
+    elif responsavel == 'jREP':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-31"]'
+    elif responsavel == 'KET':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-32"]'
+    elif responsavel == 'LBG':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-33"]'
+    elif responsavel == 'LVC':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-34"]'
+    elif responsavel == 'operacoes':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-35"]'
+    elif responsavel == 'PLV':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-36"]'
+    elif responsavel == 'PUB':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-37"]'
+    elif responsavel == 'Robô':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-38"]'
+    elif responsavel == 'SMF':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-39"]'
+    elif responsavel == 'STJ':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-40"]'
+    elif responsavel == 'TESTE':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-41"]'
+    elif responsavel == 'TLA -':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-42"]'
+    elif responsavel == 'TRIA':
+        return '//*[@id="ui-multiselect-slcResponsavel-option-43"]'
+
+def acessToIntegra():
+    # acessando a primeira página do sistema promad
+    driver.get('http://www.integra.adv.br/')
+    driver.maximize_window()
+
+    # realizando o login no sistema
+
+    element = waitinstance(driver, "login_email", 30, 1, 'show', 'id')
+    element.send_keys('robo@dplaw.com.br')
+    driver.find_element_by_id("login_senha").send_keys('dplaw00612')
+    driver.find_element_by_tag_name('button').click()
+
+def inserirVolumetria(pasta = '01700117977'):
+
+    #TODO FAZER LOOPING
+    # acessando a pesquisa de clientes no sistema
+    element_over = waitinstance(driver, "//*[@id='header']/ul/li[1]/a", 30, 1, 'click')
+    hover = ActionChains(driver).move_to_element(element_over)
+    hover.perform()
+
+    element = waitinstance(driver, "//*[@id='header']/ul/li[1]/ul/lii[1]/p", 30, 1, 'click')
+    element.click()
+    #driver.find_element_by_xpath("//*[@id='header']/ul/li[1]/ul/lii[1]/p").click()
+
+    # selecionar opção pesquisa por pasta
+    element = waitinstance(driver, '//*[@id="chkPesquisa139"]', 30, 1, 'show')
+    element.click()
+
+    # buscando pasta
+    element = waitinstance(driver, "txtPesquisa", 30, 1, 'show', 'id')
+    element.send_keys(pasta)
+    driver.find_element_by_id("btnPesquisar").click()
+    
+    # SELECIONA O CLIENTE PESQUISADO
+    time.sleep(3)
+    element = waitinstance(driver, "//*[@id='divCliente']/div[3]/table/tbody/tr/td[5]", 30, 1, 'click')
+    element.click()
+
+    # PREENCHE O CAMPO 3
+    element = waitinstance(driver, '//*[@id="txtCampoLivre3"]', 30, 1, 'show')
+    element.send_keys('volumetria.09.2018')
+
+    # SALVAR ALTERAÇÃO
+    time.sleep(2)
+    element = waitinstance(driver, '//*[@id="btnSalvar"]', 30, 1, 'show')
+    element.click()
+
+    # SALVAR ALTERAÇÃO
+    time.sleep(2)
+    element = waitinstance(driver, '//*[@id="popup_ok"]', 30, 1, 'show')
+    element.click()
+    
+driver = iniciaWebdriver()
+acessToIntegra()
+inserirVolumetria()
