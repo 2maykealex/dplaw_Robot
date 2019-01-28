4886ba7fbdc1c486e61f7040c2b69db4e2682946
Mayke Alex <mayke.suporte@gmail.com>
2019-01-28 11:06:53 -0400

    Update volumetria.py

--- volumetria.py
+++ volumetria.py
@@ -123,7 +123,8 @@ def enviaParametros(volumetriaMes, item = 1):
 
 #============================PROGRAMA PRINCIPAL==============================
 #executando python volumetria.py "Volumetria 2018.09.xlsx" no TERMINAL
-
+path = os.getcwd()
+print (path)
 path = os.getcwd() + "\\files\\volumetrias" # obtem o caminho do script e add a pasta volumetrias
 logsPath = os.getcwd() + "\\files\\volumetrias\\logs"
 
