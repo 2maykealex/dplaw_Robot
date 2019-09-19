9992e8450012e09b38b704d2ff257f32c9a05457
Mayke Alex <mayke.suporte@gmail.com>
2019-09-19 08:28:32 -0400

    ClosePath      = os.getcwd() + "\\files\\fechamento"

--- robo_monitor.py
+++ robo_monitor.py
@@ -6,17 +6,17 @@ import robot_functions as rf
 #============================ ROBO PRINCIPAL==============================
 
 localRobot        = os.getcwd() # obtem o caminho do Robô
-local_OpenFolder  = os.getcwd() + "\\abertura.py" # obtem o caminho do script Abertura de pastas
-local_Volumetria  = os.getcwd() + "\\Volumetria.py" # obtem o caminho do script Volumetria de pastas
+local_OpenFolder  = os.getcwd() + "\\abertura.py"    # obtem o caminho do script Abertura de pastas
+local_Volumetria  = os.getcwd() + "\\Volumetria.py"  # obtem o caminho do script Volumetria de pastas
 local_Update      = os.getcwd() + "\\atualizacao.py" # obtem o caminho do script Atualização de pastas
-local_CloseFolder = os.getcwd() + "\\fechamento.py" # obtem o caminho do script Encerramento de pastas
+local_CloseFolder = os.getcwd() + "\\fechamento.py"  # obtem o caminho do script Encerramento de pastas
 
 OpenFolderPath = os.getcwd() + "\\files\\abertura_pastas" # obtem o caminho do script e add a pasta abertura_pastas
-VolumetriaPath = os.getcwd() + "\\files\\volumetrias" # obtem o caminho do script e add a pasta abertura_pastas
-UpdatePath     = os.getcwd() + "\\files\\atualizacao" # obtem o caminho do script e add a pasta abertura_pastas
-ClosePath     = os.getcwd() + "\\files\\fechamento" # obtem o caminho do script e add a pasta abertura_pastas
+VolumetriaPath = os.getcwd() + "\\files\\volumetrias"     # obtem o caminho do script e add a pasta abertura_pastas
+UpdatePath     = os.getcwd() + "\\files\\atualizacao"     # obtem o caminho do script e add a pasta abertura_pastas
+ClosePath      = os.getcwd() + "\\files\\fechamento"      # obtem o caminho do script e add a pasta abertura_pastas
 
-while True: #Fará looping infinito, buscando novos arquivos nas pastas - se encontrar, abrirá o seu respectivo script
+while True:      #Fará looping infinito, buscando novos arquivos nas pastas - se encontrar, abrirá o seu respectivo script
     time.sleep(3)
     hora = time.strftime("%H:%M:%S")
     print('{} - VERIFICANDO SE HÁ NOVOS ARQUIVOS!'.format(hora))
@@ -24,22 +24,22 @@ while True: #Fará looping infinito, buscando novos arquivos nas pastas - se enc
     files =  {}
     for file in glob.glob("{}\\*.xlsx".format(OpenFolderPath)):
         fileName = file.split("\\")
-        fileName = fileName[-1] #obtem o ultimo elemento da lista, no caso, o nome do arquivo        
+        fileName = fileName[-1] #obtem o ultimo elemento da lista, no caso, o nome do arquivo
         files[OpenFolderPath] = fileName
 
     for file in glob.glob("{}\\*.xlsx".format(VolumetriaPath)):
         fileName = file.split("\\")
-        fileName = fileName[-1] #obtem o ultimo elemento da lista, no caso, o nome do arquivo        
+        fileName = fileName[-1] #obtem o ultimo elemento da lista, no caso, o nome do arquivo
         files[VolumetriaPath] = fileName
 
     for file in glob.glob("{}\\*.xlsx".format(UpdatePath)):
         fileName = file.split("\\")
-        fileName = fileName[-1] #obtem o ultimo elemento da lista, no caso, o nome do arquivo        
+        fileName = fileName[-1] #obtem o ultimo elemento da lista, no caso, o nome do arquivo
         files[VolumetriaPath] = fileName
 
     for file in glob.glob("{}\\*.xlsx".format(ClosePath)):
         fileName = file.split("\\")
-        fileName = fileName[-1] #obtem o ultimo elemento da lista, no caso, o nome do arquivo        
+        fileName = fileName[-1] #obtem o ultimo elemento da lista, no caso, o nome do arquivo
         files[VolumetriaPath] = fileName
     
     if (files):
@@ -67,14 +67,14 @@ while True: #Fará looping infinito, buscando novos arquivos nas pastas - se enc
             except:
                 pass
 
-            if (not(os.path.isfile(logFile))):  #se o log não existir - executa o arquivo no script
+            if (not(os.path.isfile(logFile))):      #se o log não existir - executa o arquivo no script
                 print("\n{} - Uma nova instancia de {} foi aberta".format(hora, folderName.upper()))
                 if (folderName == "abertura_pastas"):
-                    os.startfile('abertura.py')   #executa outro script em outro terminal - para trabalhar de forma isolada
+                    os.startfile('abertura.py')     #executa outro script em outro terminal - para trabalhar de forma isolada
                 elif (folderName == "volumetrias"):
                     os.startfile('volumetria.py')   #executa outro script em outro terminal - para trabalhar de forma isolada
                 elif (folderName == "atualizacao"):
-                    os.startfile('atualizacao.py')   #executa outro script em outro terminal - para trabalhar de forma isolada
+                    os.startfile('atualizacao.py')  #executa outro script em outro terminal - para trabalhar de forma isolada
                 elif (folderName == "fechamento"):
                     os.startfile('fechamento.py')   #executa outro script em outro terminal - para trabalhar de forma isolada
             else:
