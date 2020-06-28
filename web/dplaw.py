0eb4ea790f72eb7aba4734a6add8d1cd26a9126b
2maykealex <mayke.suporte@gmail.com>
2020-06-28 14:30:58 -0400

    rotas principais

--- web/dplaw.py
+++ web/dplaw.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return redirect(url_for('home'))
+
+@app.route("/home")
 def home():
     return render_template('home.html')
 
 @app.route("/abertura")
 def abertura():
     return render_template('abertura.html')
+
+@app.route("/abertura/bradesco")
+def abertura_bradesco():
+    return render_template('abertura_bradesco.html')
+
+@app.route("/atualizacao")
+def atualizacao():
+    return render_template('atualizacao.html')
+
+@app.route("/contrato")
+def contrato():
+    return render_template('contrato.html')
+
+@app.route("/volumetria")
+def volumetria():
+    return render_template('volumetria.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
