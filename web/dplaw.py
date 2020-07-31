f8e0d5d458b923b3ac699606d6e443c8593d253f
2maykealex <mayke.suporte@gmail.com>
2020-07-31 14:44:59 -0400

    abertura_executa

--- web/dplaw.py
+++ web/dplaw.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, json
 from pprint import pprint
 from os import getcwd
 from os import path
+from integra_functions import IntegraFunctions
 # from basic_functions import abreArquivo
 
 app = Flask(__name__)
@@ -73,6 +74,28 @@ def abertura_default2():
     data = data['txtAbertura']
     data = json.loads(data)
     return render_template('abertura_default_2.html', data=data, clientes=clientes, gruposprocessos=gruposprocessos, localizadores=localizadores, resp1=resp1, resp2=resp2, resp3=resp3, status=status, varas=varas, locaistramites=locaistramites)
+
+@app.route("/abertura/executa", methods=['POST'])
+def abertura_executa():
+    data = request.form.to_dict()
+    data = data['txtAbertura']
+    data = json.loads(data)
+
+
+    data['tipo'] = 'abertura'
+    # print(data)
+    integra = IntegraFunctions()
+    integra.controle(data)
+
+
+
+
+
+    return render_template('atualizacao.html')
+
+# @app.route("/abertura/monitoramento", methods='POST')
+# def abertura_monitoramento():
+#     return render_template('atualizacao.html')
 
 @app.route("/atualizacao")
 def atualizacao():
