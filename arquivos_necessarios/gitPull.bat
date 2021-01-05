57c054ceff59dc7c40c5cfaf796633601fd37b93
2maykealex <mayke.suporte@gmail.com>
2021-01-05 16:52:07 -0400

    Update gitPull.bat

--- arquivos_necessarios/gitPull.bat
+++ arquivos_necessarios/gitPull.bat
@@ -1,4 +1,4 @@
 TASKKILL /IM py.exe
 git pull
-start C:\DPLAW_DESENVOLVIMENTO\WEB_Dplaw_Robot\robo_monitor.py
 start C:\DPLAW_DESENVOLVIMENTO\WEB_Dplaw_Robot\webDplaw.py
+start C:\DPLAW_DESENVOLVIMENTO\WEB_Dplaw_Robot\robo_monitor.py
