43156d82b2b8ded57b279a4720df6ee045ef8fc0
2maykealex <mayke.suporte@gmail.com>
2021-01-05 16:40:52 -0400

    Create gitPull.bat

---
+++ arquivos_necessarios/gitPull.bat
@@ -0,0 +1,5 @@
+TASKKILL /IM py.exe
+git pull
+TIMEOUT 3
+start C:\DPLAW_DESENVOLVIMENTO\WEB Dplaw Robot\dplaw_Robot\robo_monitor.py
+start C:\DPLAW_DESENVOLVIMENTO\WEB Dplaw Robot\dplaw_Robot\webDplaw.py
