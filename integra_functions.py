3752f552c9d4638e9a0a0bf7182f398ad9a3fba7
2maykealex <mayke.suporte@gmail.com>
2021-02-14 21:19:26 -0400

    Reativado Conferências

--- integra_functions.py
+++ integra_functions.py
@@ -240,7 +240,7 @@ class IntegraFunctions(object):
                         robo = self.abrePasta(registros, reg)
                     else:
                         print('{} <<<NÃO HÁ MAIS REGISTROS NESSE ARQUIVO PARA IMPORTAR! >>>'.format(self.fileName).upper())
-                        basic_functions.createLog(self.logFileCSV, "\nCONFERENCIA", printOut=False)
+                        basic_functions.createLog(self.logFileCSV, "\n\nCONFERENCIA", printOut=False)
                         reg = 'CONFERENCIA'
 
                 if (robo or reg in ['CONFERENCIA', 'CONF']):
@@ -250,39 +250,39 @@ class IntegraFunctions(object):
                     if (not(path.isfile(logBackup))):
                         copy(logFileCSV, logBackup)
 
-                    # print('\n=============== CONFERÊNCIA DE DADOS ===============')
-                    # _abreWebDriver = self.acessToIntegra(self.login, self.password)
-                    # reg = 1
-                    # while True:
-                    #     if (reg > len(registros['registros'])):
-                    #         break
-                    #     registro = registros['registros']['{}'.format(reg)]
-                    #     try:
-                    #         countChar = len(str(registro['txtPasta'] if ('txtPasta' in registro) else registro['txtNroProcesso']))
-                    #         if (countChar >= 14):
-                    #             searchFolder, elementoPesquisado = self.realizarPesquisa(registro['txtNroProcesso'] if (registro['txtPasta'] in registro) else registro['txtPasta'], 'processo')  # INVERTIDO
-                    #         else:
-                    #             searchFolder, elementoPesquisado = self.realizarPesquisa(registro['txtPasta'], 'pasta')
-                    #     except:
-                    #         return False
+                    print('\n=============== CONFERÊNCIA DE DADOS ===============')
+                    _abreWebDriver = self.acessToIntegra(self.login, self.password)
+                    reg = 1
+                    while True:
+                        if (reg > len(registros['registros'])):
+                            break
+                        registro = registros['registros']['{}'.format(reg)]
+                        try:
+                            countChar = len(str(registro['txtPasta'] if ('txtPasta' in registro) else registro['txtNroProcesso']))
+                            if (countChar >= 14):
+                                searchFolder, elementoPesquisado = self.realizarPesquisa(registro['txtNroProcesso'] if (registro['txtPasta'] in registro) else registro['txtPasta'], 'processo')  # INVERTIDO
+                            else:
+                                searchFolder, elementoPesquisado = self.realizarPesquisa(registro['txtPasta'], 'pasta')
+                        except:
+                            return False
 
-                    #     if (searchFolder):
-                    #         elementoPesquisado.click() # Na conferência, sempre vai clicar
-                    #         basic_functions.createLog(self.logFileCSV, "\n", printOut=False) #pula linha no Log
-                    #         message = self.incluiAlteraProcesso(registro, reg, registros['tipo'], check=True)
-                    #         # confereAgendamentos = self.criaAgendammentos(registro, reg, True)
-                    #         # if (confereAgendamentos): message = '{}{}'.format(message, confereAgendamentos)
+                        basic_functions.createLog(self.logFileCSV, "\n", printOut=False)
+                        if (searchFolder):
+                            elementoPesquisado.click() # Na conferência, sempre vai clicar
+                            message = self.incluiAlteraProcesso(registro, reg, registros['tipo'], check=True)
+                            # confereAgendamentos = self.criaAgendammentos(registro, reg, True)
+                            # if (confereAgendamentos): message = '{}{}'.format(message, confereAgendamentos)
 
-                    #         if (message):
-                    #             if (message == True): message = 'NENHUM ITEM PRECISOU DE CORREÇÃO!'
-                    #             basic_functions.createLog(self.logFileCSV, "CONF REG {};;{};{};{}\n".format(reg, registro['txtPasta'] if ('txtPasta' in registro) else registro['txtNroProcesso'], "FOI CHECADO", message), printOut=False)
-                    #         else:
-                    #             basic_functions.createLog(self.logFileCSV, "CONF REG {};;{};{}\n".format(reg, registro['txtPasta'] if ('txtPasta' in registro) else registro['txtNroProcesso'], "NÃO FOI POSSIVEL CHECAR ESSA PASTA - CHECAR MANUALMENTE!"), printOut=False)
-                    #     else:
-                    #         basic_functions.createLog(self.logFileCSV, "CONF REG {};;{};{}\n".format(reg, registro['txtPasta'] if ('txtPasta' in registro) else registro['txtNroProcesso'], "NÃO FOI POSSIVEL CHECAR ESSA PASTA - CHECAR MANUALMENTE!"), printOut=False)
-                    #     reg = reg + 1
+                            if (message):
+                                if (message == True): message = 'NENHUM ITEM PRECISOU DE CORREÇÃO!'
+                                basic_functions.createLog(self.logFileCSV, "CONF REG {};;{};{};{}".format(reg, registro['txtPasta'] if ('txtPasta' in registro) else registro['txtNroProcesso'], "FOI CHECADO", message), printOut=False)
+                            else:
+                                basic_functions.createLog(self.logFileCSV, "CONF REG {};;{};{}".format(reg, registro['txtPasta'] if ('txtPasta' in registro) else registro['txtNroProcesso'], "NÃO FOI POSSIVEL CHECAR ESSA PASTA - CHECAR MANUALMENTE!"), printOut=False)
+                        else:
+                            basic_functions.createLog(self.logFileCSV, "CONF REG {};;{};{}".format(reg, registro['txtPasta'] if ('txtPasta' in registro) else registro['txtNroProcesso'], "NÃO FOI POSSIVEL CHECAR ESSA PASTA - CHECAR MANUALMENTE!"), printOut=False)
+                        reg = reg + 1
 
-                    basic_functions.createLog(self.logFileCSV, "\nFIM", printOut=False)
+                    basic_functions.createLog(self.logFileCSV, "\nFIM;", printOut=False)
                     self.logoutIntegra()
                     return True
                 else:
@@ -405,8 +405,7 @@ class IntegraFunctions(object):
                 reg = reg + 1
 
             print('{}<<<<< NÃO HÁ MAIS REGISTROS PARA IMPORTAR. FINALIZANDO! >>>>>'.format(self.fileName))
-            basic_functions.createLog(self.logFileCSV, "\nFIM", printOut=False)
-            # basic_functions.createLog(self.logFileCSV, "\n\nCONFERENCIA", printOut=False) #TODO     ATIVAR NA CONFERÊNCIA
+            basic_functions.createLog(self.logFileCSV, "\n\nCONFERENCIA;", printOut=False)
 
         except Exception as err:
             exception_type, exception_object, exception_traceback = exc_info()
